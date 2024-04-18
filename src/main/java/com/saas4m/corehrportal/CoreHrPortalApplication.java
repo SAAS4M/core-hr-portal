8dbbae9f6989a46eb7e4a9c4a0da3b4e872df15a
mrahmanashiq <mizanur.r.ashiq@gmail.com>
2024-04-19 00:09:03 +0600

    init :airplane:

---
+++ src/main/java/com/saas4m/corehrportal/CoreHrPortalApplication.java
@@ -0,0 +1,13 @@
+package com.saas4m.corehrportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreHrPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreHrPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saas4m/corehrportal/CoreHrPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saas4m.corehrportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreHrPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
